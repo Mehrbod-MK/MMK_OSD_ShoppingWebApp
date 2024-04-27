3032812930b0ca766bf3ede5a0538ada25ddd4d0
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-04-27 11:38:55 +0330

    Added 'Product' DB model.

--- mmk_osd_shoppingwebsite/shop/models.py
+++ mmk_osd_shoppingwebsite/shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO: Implement arrays.
+class Product(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    price = models.PositiveBigIntegerField()
+    quantityAvailable = models.PositiveIntegerField()
+    descriptions = models.TextField(null=True)
+
