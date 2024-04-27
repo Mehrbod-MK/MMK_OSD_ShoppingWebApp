ef6ec6c62dd3fdaab2a01d37c7736ad43200cdca
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-04-27 23:18:06 +0330

    Made a 'very' simple login authentication.

--- mmk_osd_shoppingwebsite/shop/urls.py
+++ mmk_osd_shoppingwebsite/shop/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='mmk-shop-login'),
+    path('userAuth/', views.userAuth, name='mmk-shop-userAuth'),
+
     path('', views.index, name='mmk-shop-index'),
 ]
--- mmk_osd_shoppingwebsite/shop/views.py
+++ mmk_osd_shoppingwebsite/shop/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+def userAuth(request):
+    user_Username = request.POST["user_Username"]
+    user_Password = request.POST["user_Password"]
+
+    user = authenticate(request=request, username=user_Username, password=user_Password)
+    if(user is not None):
+        return HttpResponse('OK!')
+    else:
+        return HttpResponse('No such user!')
