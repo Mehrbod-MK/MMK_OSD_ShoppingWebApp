665d3013eedfa98d7e680e2c77bcce308ca398d4
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-04-29 20:36:31 +0330

    Added 'CategoryEnum' enumeration to models.

--- mmk_osd_shoppingwebsite/shop/models.py
+++ mmk_osd_shoppingwebsite/shop/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+## CategoryEnum enumeration.
+class CategoryEnum(models.Model):
+    name = models.CharField(unique=True, max_length=70)
+
 # TODO: Implement arrays.
 class Product(models.Model):
     name = models.CharField(unique=True, max_length=100)
     price = models.PositiveBigIntegerField()
     quantityAvailable = models.PositiveIntegerField()
     descriptions = models.TextField(null=True)
+    category = models.ForeignKey(CategoryEnum, on_delete=models.CASCADE, default=0)
 
